3a12b155573afbca76b22c7e5744174e8b424f38
Yassoea <elakhalyassine10@gmail.com>
2024-04-03 14:40:40 +0200

    Best Website ever

--- script.js
+++ script.js
@@ -1,58 +1,57 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var anchors = document.querySelectorAll('.side-panel a');
+    const $body = document.body;
+    const $menu_trigger = $body.querySelector('.menu-trigger');
+    const $side_panel = $body.querySelector('.side-panel');
+    const $close_button = $side_panel.querySelector('.close-button'); 
 
-    anchors.forEach(function(anchor) {
-        anchor.addEventListener('click', function(event) {
-            // Check if the anchor link points to an external page
-            if (this.getAttribute('href').startsWith('#')) {
-                // Prevent the default anchor behavior for internal links
-                event.preventDefault();
-                var targetId = this.getAttribute('href').substring(1);
-                var targetElement = document.getElementById(targetId);
-                targetElement.scrollIntoView({ behavior: 'smooth' });
-                document.body.classList.remove('menu-active');
+    const adjustMenuTriggerPosition = () => {
+        const scrollPos = window.scrollY;
+        const minimumTop = 50; 
+
+        if ($menu_trigger) {
+            if (scrollPos > $side_panel.offsetTop) {
+                $menu_trigger.style.position = 'fixed';
+                $menu_trigger.style.top = '20px'; 
+            } else {
+                $menu_trigger.style.position = 'absolute';
+                $menu_trigger.style.top = minimumTop + 'px'; 
             }
-            // For external links, let the default behavior occur (i.e., navigating to another page)
-        });
-    });
+        }
+    };
 
-    var $body = document.body;
-    var $menu_trigger = $body.querySelector('.menu-trigger');
-    var $side_panel = $body.querySelector('.side-panel');
-    var $close_button = $side_panel.querySelector('.close-button'); 
+    const smoothScrollToTarget = (targetId) => {
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+            $body.classList.remove('menu-active');
+        }
+    };
+
+    const handleClick = (event) => {
+        const href = event.target.getAttribute('href');
+        if (href && href.startsWith('#')) {
+            event.preventDefault();
+            const targetId = href.substring(1);
+            smoothScrollToTarget(targetId);
+        }
+    };
 
     if ($menu_trigger) {
-        $menu_trigger.addEventListener('click', function() {
+        $menu_trigger.addEventListener('click', () => {
             $body.classList.toggle('menu-active');
         });
     }
 
     if ($close_button) {
-        $close_button.addEventListener('click', function() {
+        $close_button.addEventListener('click', () => {
             $body.classList.remove('menu-active'); 
         });
     }
-    adjustMenuTriggerPosition();
 
-    window.addEventListener('scroll', function() {
-        adjustMenuTriggerPosition();
+    adjustMenuTriggerPosition();
+    window.addEventListener('scroll', adjustMenuTriggerPosition);
+
+    $body.querySelectorAll('.side-panel a').forEach(anchor => {
+        anchor.addEventListener('click', handleClick);
     });
 });
-
-function adjustMenuTriggerPosition() {
-    var $body = document.body;
-    var $menu_trigger = $body.querySelector('.menu-trigger');
-    var $side_panel = $body.querySelector('.side-panel');
-    var scrollPos = window.scrollY;
-    var minimumTop = 50; 
-
-    if ($menu_trigger) {
-        if (scrollPos > $side_panel.offsetTop) {
-            $menu_trigger.style.position = 'fixed';
-            $menu_trigger.style.top = '20px'; 
-        } else {
-            $menu_trigger.style.position = 'absolute';
-            $menu_trigger.style.top = minimumTop + 'px'; 
-        }
-    }
-}
